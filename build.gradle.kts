5c4e1e12494f2ddfceb60b2172c9d9b2de2dbcbf
Filipe Freitas <ffreitas@cc.isel.ipl.pt>
2022-03-07 14:05:30 +0000

    Changed build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,3 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
-group = "pt.isel.ls"
-version = "0.0.0"
-
 plugins {
     kotlin("jvm") version "1.6.10"
 }
@@ -14,14 +9,6 @@ repositories {
 dependencies {
     implementation(group = "org.postgresql", name = "postgresql", version = "42.+")
     testImplementation(kotlin("test"))
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-
-tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "11"
 }
 
 tasks.register<Copy>("copyRuntimeDependencies") {
